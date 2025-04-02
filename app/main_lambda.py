4bcc646e9200d154814fa12bc1a7b47c0d8f183b
bnuazz15 <bnuazz15@gmail.com>
2025-04-02 22:52:35 +0900

    lambda 코드 추가

--- app/main_lambda.py
+++ app/main_lambda.py
@@ -1,7 +1,5 @@
 from datetime import datetime, date, timedelta
-import requests
-import json
-import math
+import os
 
 from app.utils.database import get_db, get_db_session
 from app.utils.crud_sql import SQLExecutor
@@ -9,41 +7,17 @@ from app.utils.auto_trading_bot import AutoTradingBot
 from app.utils.dynamodb.model.stock_symbol_model import StockSymbol
 from app.utils.dynamodb.model.user_info_model import UserInfo
 
-# db = get_db()
-sql_executor = SQLExecutor()
 
-
-def scheduled_trading_schedulerbot_task():
-    scheduled_trading(id="schedulerbot")
-
-def scheduled_trading_id1_task():
-    scheduled_trading(id="id1")
-
-def scheduled_trading_id2_task():
-    scheduled_trading(id="id2")
-
-def scheduled_trading_bnuazz15_task():
-    scheduled_trading(id="bnuazz15")
-
-
-def send_discord_webhook(message, bot_type):
-    if bot_type == 'trading':
-        webhook_url = 'https://discord.com/api/webhooks/1313346849838596106/6Rn_8BNDeL9bMYfFtqscpu4hPah5c2RsNl0rBiPoSw_Qb9RXgDdVHoHmwEuStPv_ufnV'  # 복사한 Discord 웹훅 URL로 변경
-        username = "Stock Trading Bot"
-
-    data = {
-        "content": message,
-        "username": username,  # 원하는 이름으로 설정 가능
+def lambda_handler(event, context):
+    print("Lambda Docker container is running!")
+    id = os.environ.get("ID", "schedulerbot")  # 환경 변수가 없으면 None 반환
+    
+    scheduled_trading(id)
+    
+    return {
+        "statusCode": 200,
+        "body": "Hello from Lambda in Docker!"
     }
-    
-    # 요청 보내기
-    response = requests.post(webhook_url, json=data)
-    
-    # 응답 확인
-    if response.status_code == 204:
-        print("메시지가 성공적으로 전송되었습니다.")
-    else:
-        print(f"메시지 전송 실패: {response.status_code}, {response.text}")
 
 
 def scheduled_trading(id):
@@ -52,18 +26,6 @@ def scheduled_trading(id):
     # 매수 로직 여기에 추가
     trading_bot = AutoTradingBot(id=id)
     
-    # sql_executor = SQLExecutor()
-
-    # query = """
-    #     SELECT 종목코드, 종목이름 FROM fsts.kospi200;
-    # """
-
-    # params = {
-    # }
-
-    # with get_db_session() as db:
-    #     result = sql_executor.execute_select(db, query, params)
-
     result = list(StockSymbol.scan(
         filter_condition=((StockSymbol.type == 'kospi200') | (StockSymbol.type == 'kosdaq150'))
     ))
